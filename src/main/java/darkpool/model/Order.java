3c3640b3644362f52713f868e5acfad621ca1854
joaobitencourt <jobitencourt924@gmail.com>
2026-08-28 18:54:03 -0300

    first commit

---
+++ src/main/java/darkpool/model/Order.java
@@ -0,0 +1,10 @@
+public class Order{
+
+    public int orderId = 0;
+    public long symbol = 0L;
+    public long price = 0L;
+    public int quantity = 0;
+    public int remainingQuantity = 0;
+    public byte side = 0;
+
+}
